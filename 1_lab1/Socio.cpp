b51b05050d9b0a74fde1bdceaf4992352c0b623c
aldanarodrigu <aldanarodriguez5407@gmail.com>
2025-03-23 19:41:25 -0300

    Agregue clase Socio y funciones

---
+++ 1_lab1/Socio.cpp
@@ -0,0 +1,28 @@
+#include "Socio.h"
+#include <iostream>
+
+Socio::Socio(string n, string c){
+    this->nombre = n;
+    this->ci = c;
+}
+
+string Socio::getNombre(){
+    return this->nombre;
+}
+
+string Socio::getCi(){
+    return this->ci;
+}
+
+void Socio::setNombre(string n){
+    this->nombre = n;
+}
+
+void Socio::setCi(string c){
+    this->ci = c;
+}
+
+void Socio::mostrarSocio(){
+    cout<<"Nombre: "<<nombre<<endl;
+    cout<<"CI: "<<ci<<endl;
+}
---
+++ 1_lab1/Socio.h
@@ -0,0 +1,23 @@
+#ifndef SOCIO_H
+#define SOCIO_H
+
+#include <iostream>
+using namespace std;
+
+class Socio{
+    private:
+        string ci;
+        string nombre;
+    public:
+        Socio(string, string);
+
+        string getNombre();
+        string getCi();
+
+        void setNombre(string);
+        void setCi(string);
+        
+        void mostrarSocio();
+};
+
+#endif
