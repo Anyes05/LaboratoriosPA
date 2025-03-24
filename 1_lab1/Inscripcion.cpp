77b8ffa7aab67c0da98878f4ca12ad44d53cfda9
AguB19 <agustinbraggio1521@gmail.com>
2025-03-23 22:59:40 -0300

    Inscripcion.h e Inscripcion.cpp

---
+++ 1_lab1/Inscripcion.cpp
@@ -0,0 +1,46 @@
+#include "Inscripcion.h"
+#include <iostream>
+
+using namespace std;
+
+Inscripcion::Inscripcion(Fecha *f, Socio* s, Clase* c) {
+    this->fecha = f;
+    this->socio = s;
+    this->clase = c;
+}
+
+Socio* Inscripcion::getSocio() {
+    return this->socio;
+}
+
+
+Clase* Inscripcion::getClase() {
+    return this->clase;
+}
+
+Fecha* Inscripcion::getFecha() {
+    return this->fecha;
+}
+
+ void Inscripcion::setFecha(Fecha *f) {
+        this->fecha = f;
+    }
+
+    void Inscripcion::setSocio(Socio* s) {
+        this->socio = s;
+    }
+
+    void Inscripcion::setClase(Clase* c) {
+        this->clase = c;
+    }
+
+
+void Inscripcion::mostrarInscripcion() {
+    cout << "Fecha de inscripción: " << fecha->getDia() << "/" 
+         << fecha->getMes() << "/" << fecha->getAnio() << endl;
+    cout << "Socio: " << socio->getNombre() << " (CI: " << socio->getCi() << ")" << endl;
+    cout << "Clase: " << clase->getNombre() << " (ID: " << clase->getId() << ")" << endl;
+}
+
+// Destructor
+Inscripcion::~Inscripcion() {}
---
+++ 1_lab1/Inscripcion.h
@@ -0,0 +1,32 @@
+#ifndef INSCRIPCION_H
+#define INSCRIPCION_H
+
+#include <iostream>
+using namespace std;
+
+#include "Clase.h"
+#include "DataTypes/Fecha.h"
+#include "Socio.h"
+
+
+class Inscripcion {
+
+    private: 
+        Fecha * fecha;
+        Socio * socio;
+        Clase * clase;
+
+    public:
+        Inscripcion (Fecha *, Socio*, Clase*);
+        Socio *getSocio();
+        Clase *getClase();
+        Fecha *getFecha();
+        void setFecha(Fecha *);
+        void setSocio(Socio *);
+        void setClase(Clase *);
+        void mostrarInscripcion();
+        ~Inscripcion();
+
+}
+
+#endif
